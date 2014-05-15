95f46ee56c1ac4185633f6f3e9719ca70971f12d
gubak <gubak@coka.rs>
2014-05-15 14:35:14 +0200

    First change

--- git2/src/git2/manager/main.java
+++ git2/src/git2/manager/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Start");
 
-		
+		System.out.println("Something has changed !!!");
 		
 		System.out.println("Stop");
 
